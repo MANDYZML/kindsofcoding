412586c859d4eadbb8b6188ccc546c5c5a13b4aa
MANDYZML <manlinzhang4@gmail.com>
2022-01-11 23:29:23 +0900

    1/11 更新

--- test1_10/test.c
+++ test1_10/test.c
@@ -14,18 +14,101 @@
 //	return 0;
 //}
 
-#include <stdio.h>
-int main()
+//#include <stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d ", &m);
+//	if (m % 5 == 0)
+//	{
+//		printf("YES\n");
+//	}
+//	else
+//	{
+//		printf("NO\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("\a\a\a\a\a");
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int max(int x, int y)
+//{
+//	if (x > y)
+//	{
+//		return x;
+//	}
+//	else
+//		return y;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a,&b);
+//	int n = max(a, b);
+//	printf("%d\n", n);
+//	return 0;
+//}
+// 
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    int m = a / b;
+//    int n = a % b;
+//    printf("%d %d", m, n);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a = 40;
+//    int c = 212;
+//    int b = (-8 + 22) * a - 10 + c / 2;
+//    printf("%d\n", b);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    scanf("%d\n", &a);
+//    if (a >= 140)
+//    {
+//        printf("Genius\n");
+//    }
+//    return 0;
+//}
+
+#include<stdio.h>
+int max(int x, int y)
 {
-	int m = 0;
-	scanf("%d ", &m);
-	if (m % 5 == 0)
+	if (x > y)
 	{
-		printf("YES\n");
+		return x;
 	}
 	else
-	{
-		printf("NO\n");
-	}
+		return y;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int m = max(a, b);
+	printf("%d\n", m);
 	return 0;
 }
