6020c9bde7ac9bf30e006931d1192d87250305e2
MANDYZML <manlinzhang4@gmail.com>
2022-01-25 23:39:30 +0900

    1/25 更新

---
+++ game3/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void menu()
+{
+	printf("*************************\n");
+	printf("******  0.exit  *********\n");
+	printf("******  1.play  *********\n");
+	printf("*************************\n");
+
+}
+int main()
+{
+	do
+	{
+		menu();
+	
+	} while ();
+	return 0;
+}
