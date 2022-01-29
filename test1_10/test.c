deab67cc6ddd283c3bd001af4110c85999bcf303
MANDYZML <manlinzhang4@gmail.com>
2022-01-29 23:39:38 +0900

    1/29  更新

--- test1_10/test.c
+++ test1_10/test.c
@@ -1217,19 +1217,107 @@
 
 //将数组A中的内容和数组B中的内容进行交换。（数组一样大）
 //数组名是地址
+//#include <stdio.h>
+//int main()
+//{
+//	int arr1[] = { 1,3,5,7,9 };
+//	int arr2[] = { 2,4,6,8,0 };
+//
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		int tmp = arr1[i];//一个一个交换
+//		arr1[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//	return 0;
+//}
+
+
+//求两个数二进制中不同位的个数
+//编程实现：两个int（32位）整数m和n的二进制表达中，有多少个位(bit)不同？
+//输入例子 :
+//1999 2299
+//输出例子 : 7
+
+//#include <stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d %d", &m, &n);
+//	int a = m ^ n;
+//	int count = 0;
+//	int i = 0;
+//	for(i = 0; i < 32; i++)
+//	{
+//		if ((a & 1)==1)
+//		{
+//			count++;
+//		}
+//		a=a >> 1;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+//打印整数二进制的奇数位和偶数位
+//获取一个整数二进制序列中所有的偶数位和奇数位，分别打印出二进制序列
+#include <stdio.h>
+//void printbit(int num)
+//{
+//	int i = 0;
+//	for (i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d ", (num >> i) & 1);
+//	}
+//	printf("\n");
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d ", (num >> i) & 1);
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int odd = 0;//奇数
+//	int even = 0;//偶数
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((num & 1) == 1)
+//		{
+//			odd++;
+//		}
+//		else
+//			even++;
+//		num = num >> 1;
+//	}
+//	printf("奇数位：%d\n偶数位：%d\n", odd, even);
+//	printbit(num);
+//	return 0;
+//}
+
+
+//输入一个整数 n ，输出该数32位二进制表示中1的个数。其中负数用补码表示。
 #include <stdio.h>
 int main()
 {
-	int arr1[] = { 1,3,5,7,9 };
-	int arr2[] = { 2,4,6,8,0 };
-
-	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int m = 0;
+	scanf("%d", &m);
 	int i = 0;
-	for (i = 0; i < sz; i++)
+	int count = 0;
+	for (i = 0; i < 32; i++)
 	{
-		int tmp = arr1[i];//一个一个交换
-		arr1[i] = arr2[i];
-		arr2[i] = tmp;
+		if ((m & 1) == 1)
+		{
+			count++;
+		}
+		m = m >> 1;
 	}
+	printf("%d个1\n", count);
 	return 0;
 }
