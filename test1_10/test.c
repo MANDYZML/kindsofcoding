8c10320caaf8f65fcef7600683e200d9d01a0886
MANDYZML <manlinzhang4@gmail.com>
2022-02-27 18:02:28 +0900

    2/27 更新

--- test1_10/test.c
+++ test1_10/test.c
@@ -1439,11 +1439,55 @@
 //来调整该数组中数字的顺序使得数组中所有的奇数位于数组的前半部分，
 //所有偶数位于数组的后半部分。
 
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	
+//
+//
+//	return 0;
+//}
+
+
+//模拟实现库函数strlen
+#include<stdio.h>
+#include<assert.h>
+//int my_strlen(const char*str)
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str != '\0')
+//	{
+//		str++;
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int len = my_strlen("abcdef");
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+//模拟实现库函数strcpy
+char*my_strcpy(char*dest,const char*src)
+{
+	assert(dest && src);
+	char* ret = dest;
+	while (*dest++ = *src++)
+	{
+		;
+	}
+	return ret;
+}
 int main()
 {
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	
+	char arr1[] = "hello bit";
+	char arr2[] = "xxxxxxxxxxxxxxxxxxx";
+	char* p = NULL;
 
+	printf("%s\n", my_strcpy(arr2, arr1));
 
 	return 0;
 }
